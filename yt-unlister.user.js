0e83e3a9e9b8bc97c2eba608a808e3221f4c0cdb
Gnlow <gnlow@pm.me>
2025-07-26 03:27:07 +0900

    Create yt-unlister.user.js

---
+++ yt-unlister.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         yt-unlister
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  Intercepts YouTube URL changes to remove the 'list' parameter.
+// @author       Gnlow
+// @match        *://*.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    "use strict"
+
+    const onLoad = () => {
+        alert("load")
+        const newLoc = window.location.search
+          .replace(/(?<=[?&])list=[^&]+/, "")
+          .replace("&&", "&")
+        if (window.location.search != newLoc)
+          window.location.search = newLoc
+    }
+
+    window.addEventListener("load", function() {
+        onLoad()
+    })
+
+    let lastUrl = location.href
+    new MutationObserver(() => {
+        const url = location.href
+        if (url !== lastUrl) {
+            lastUrl = url
+            onLoad()
+        }
+    }).observe(document, {subtree: true, childList: true})
+})
